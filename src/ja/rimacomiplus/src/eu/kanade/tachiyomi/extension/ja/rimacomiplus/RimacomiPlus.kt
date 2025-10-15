678562a4a97ddfd85e70a19ef2e2f036219a4d71
Manticore <133025162+manti-X@users.noreply.github.com>
2025-10-15 22:12:40 +0200

    move

--- src/ja/rimacomiplus/src/eu/kanade/tachiyomi/extension/ja/rimacomiplus/RimacomiPlus.kt
+++ src/ja/rimacomiplus/src/eu/kanade/tachiyomi/extension/ja/rimacomiplus/RimacomiPlus.kt
@@ -1,15 +1,10 @@
 package eu.kanade.tachiyomi.extension.ja.rimacomiplus
 
 import eu.kanade.tachiyomi.multisrc.comiciviewer.ComiciViewer
-import eu.kanade.tachiyomi.multisrc.comiciviewer.ViewerResponse
 import eu.kanade.tachiyomi.network.GET
-import eu.kanade.tachiyomi.source.model.Page
 import eu.kanade.tachiyomi.source.model.SManga
-import eu.kanade.tachiyomi.util.asJsoup
-import keiyoushi.utils.parseAs
 import okhttp3.HttpUrl.Companion.toHttpUrl
 import okhttp3.Request
-import okhttp3.Response
 
 class RimacomiPlus : ComiciViewer(
     "RimacomiPlus",
@@ -27,47 +22,6 @@ class RimacomiPlus : ComiciViewer(
             .build()
 
         return GET(chapterListUrl, headers)
-    }
-
-    override fun pageListParse(response: Response): List<Page> {
-        val document = response.asJsoup()
-        val viewer = document.selectFirst("#comici-viewer") ?: throw Exception("You need to log in via WebView to read this chapter or purchase this chapter")
-        val comiciViewerId = viewer.attr("comici-viewer-id")
-        val memberJwt = viewer.attr("data-member-jwt")
-        val apiHeaders = headers.newBuilder()
-            .set("Referer", response.request.url.toString())
-            .build()
-
-        val requestUrl = "$baseUrl/book/contentsInfo".toHttpUrl().newBuilder()
-            .addQueryParameter("comici-viewer-id", comiciViewerId)
-            .addQueryParameter("user-id", memberJwt)
-            .addQueryParameter("page-from", "0")
-
-        val pageTo = client.newCall(GET(requestUrl.addQueryParameter("page-to", "1").build(), apiHeaders))
-            .execute().use { initialResponse ->
-                if (!initialResponse.isSuccessful) {
-                    throw Exception("Failed to get page list")
-                }
-                initialResponse.parseAs<ViewerResponse>().totalPages.toString()
-            }
-
-        val getAllPagesUrl = requestUrl.setQueryParameter("page-to", pageTo).build()
-        return client.newCall(GET(getAllPagesUrl, apiHeaders)).execute().use { allPagesResponse ->
-            if (allPagesResponse.isSuccessful) {
-                allPagesResponse.parseAs<ViewerResponse>().result.map { resultItem ->
-                    val urlBuilder = resultItem.imageUrl.toHttpUrl().newBuilder()
-                    if (resultItem.scramble.isNotEmpty()) {
-                        urlBuilder.addQueryParameter("scramble", resultItem.scramble)
-                    }
-                    Page(
-                        index = resultItem.sort,
-                        imageUrl = urlBuilder.build().toString(),
-                    )
-                }.sortedBy { it.index }
-            } else {
-                throw Exception("Failed to get full page list")
-            }
-        }
     }
 
     override fun getFilterOptions(): List<Pair<String, String>> = listOf(
